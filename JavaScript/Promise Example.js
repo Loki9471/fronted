49a19d3c8d101a7f8a6fe8a9c049f6ee37c308f8
Loki9471 <valligatlalokesh123@gmaail.com>
2024-12-06 14:29:27 +0530

    Promise Example

---
+++ JavaScript/Promise Example.js
@@ -0,0 +1,44 @@
+// 1 example 
+const myPromise=new Promise((resolve,reject)=>{
+    setTimeout(()=> {
+        resolve("promise full after 2 seconds ");
+    },3000)
+});
+myPromise
+.then((message)=>{
+    console.log(message+" Loki");
+})
+.catch((error)=>{
+    console.log(error);
+});
+
+// example 2 
+const ex=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        reject("loki runs after 1 sec");
+    },1000)
+});
+ex
+.then((message)=>{
+    console.log(message);
+})
+.catch((error)=>{
+    console.log(error+" Loki");
+});
+
+// 3 example 
+const Number=new Promise((resolve,reject)=>{
+    let a=Math.random();
+    if(a>0){
+        resolve("number is greater than zero");
+    }else{
+        reject("not ")
+    }
+});
+Number
+.then((message)=>{
+   console.log(message);
+})
+.catch((error)=>{
+    console.log(error);
+});
